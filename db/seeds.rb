b98de8d7253e3fe4bc0d28edf0a10ea55936705b
wolfieorama <wachirawamunyi@gmail.com>
2016-03-17 19:31:56 +0300

    created a sed file and loaded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# #
+# # Examples:
+# #
+# #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+# #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# order1 = Order.create(total_price: 12, purchased_at: Date.parse("2016-03-12 16:18:00"), shipping: true)
+# order2 = Order.create(total_price: 44, purchased_at: Date.parse("2016-03-12 16:18:00"), shipping: false)
+# order3 = Order.create(total_price: 15, purchased_at: Date.parse("2016-03-14 16:18:00"), shipping: true)
+# order4 = Order.create(total_price: 66, purchased_at: Date.parse("2016-03-14 16:18:00"), shipping: true)
+# order5 = Order.create(total_price: 77, purchased_at: Date.parse("2016-03-14 16:18:00"), shipping: false)
+# order6 = Order.create(total_price: 8, purchased_at: Date.parse("2016-03-14 16:18:00"), shipping: false)
+# order7 = Order.create(total_price: 192, purchased_at: Date.parse("2016-03-17 16:18:00"), shipping: false)
+# order8 = Order.create(total_price: 15, purchased_at: Date.parse("2016-03-17 16:18:00"), shipping: true)
+# order9 = Order.create(total_price: 134, purchased_at: Date.parse("2016-03-17 16:18:00"), shipping: true)
+# order10 = Order.create(total_price: 100, purchased_at: Date.parse("2016-03-17 16:18:00"), shipping: true)
+# order11 = Order.create(total_price: 22, purchased_at: Date.parse("2016-03-19 16:18:00"), shipping: true)
+# order12 = Order.create(total_price: 32, purchased_at: Date.parse("2016-03-19 16:18:00"), shipping: false)
+# order13 = Order.create(total_price: 122, purchased_at: Date.parse("2016-03-19 16:18:00"), shipping: false)
+# order14 = Order.create(total_price: 177, purchased_at: Date.parse("2016-03-20 16:18:00"), shipping: false)
+# order15 = Order.create(total_price: 55, purchased_at: Date.parse("2016-03-20 16:18:00"), shipping: false)
+# order16 = Order.create(total_price: 88, purchased_at: Date.parse("2016-03-20 16:18:00"), shipping: false)
+# order17 = Order.create(total_price: 99, purchased_at: Date.parse("2016-03-22 16:18:00"), shipping: true)
+# order18 = Order.create(total_price: 23, purchased_at: Date.parse("2016-03-22 16:18:00"), shipping: true)
+# order19 = Order.create(total_price: 155, purchased_at: Date.parse("2016-03-22 16:18:00"), shipping: true)
+# order20 = Order.create(total_price: 100, purchased_at: Date.parse("2016-03-23 16:18:00"), shipping: true)
+# order21 = Order.create(total_price: 900, purchased_at: Date.parse("2016-03-23 16:18:00"), shipping: true)
+# order22 = Order.create(total_price: 182, purchased_at: Date.parse("2016-03-23 16:18:00"), shipping: true)
